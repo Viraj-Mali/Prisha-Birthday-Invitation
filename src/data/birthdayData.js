cdb808736eeaa5afc694aee70e433d2875fd81c4
Viraj Mali <maliviraj520.com>
2026-08-17 16:53:50 +0530

    Change birthday age to 1st birthday

--- src/data/birthdayData.js
+++ src/data/birthdayData.js
@@ -9,16 +9,16 @@ export const weddingData = {
   // Birthday girl info
   birthday: {
     name: "Prisha",
-    age: 3,
-    ageLabel: "3rd",
-    tagline: "Turning THREE! 🎀",
+    age: 1,
+    ageLabel: "1st",
+    tagline: "Turning ONE! 🎀",
     subtitle: "You're Invited to the Party!",
-    mainLine: "Prisha's 3rd Birthday",
+    mainLine: "Prisha's 1st Birthday",
     partyDate: "19 September 2026",
     partyTime: "4:00 PM",
     partyDateISO: "2026-09-19T16:00:00+05:30",
     timezone: "Asia/Kolkata",
-    hashtag: "#PrishaTurns3",
+    hashtag: "#PrishaTurns1",
     blessing: "🎀 A Magical Celebration 🎀",
     emoji: "🎀",
   },
@@ -36,20 +36,20 @@ export const weddingData = {
     brideParents: "",
     brideAddress: "",
     birthdayGirlName: "Prisha",
-    birthdayGirlAge: "3",
-    birthdayGirlNote: "She loves balloons, candy & dancing! 🎈",
+    birthdayGirlAge: "1",
+    birthdayGirlNote: "She loves balloons, candy & crawling around! 🎈",
   },
 
   // Reusing 'wedding' key for component compatibility
   wedding: {
     title: "Birthday Invitation",
     subtitle: "You're Invited to the Party!",
-    mainLine: "Prisha Turns 3! 🎀",
+    mainLine: "Prisha Turns 1! 🎀",
     weddingDate: "19 September 2026",
     weddingTime: "4:00 PM",
     weddingDateISO: "2026-09-19T16:00:00+05:30",
     timezone: "Asia/Kolkata",
-    hashtag: "#PrishaTurns3",
+    hashtag: "#PrishaTurns1",
     blessing: "🎈 Come Celebrate With Us! 🎈",
   },
 
@@ -71,7 +71,7 @@ export const weddingData = {
       time: "4:00 PM",
       venue: "Yashobhoomi Society",
       isoDate: "2026-09-19T16:00:00+05:30",
-      calendarTitle: "Prisha's 3rd Birthday Party",
+      calendarTitle: "Prisha's 1st Birthday Party",
       illustration: "",
       description: "Fun games, activities & balloon time!",
     },
@@ -102,7 +102,7 @@ export const weddingData = {
   ],
 
   invitationMessage:
-    "With hearts full of joy, we invite you and your little ones to celebrate our daughter's 3rd birthday! Come join us for a magical afternoon of fun, laughter, cake, and lots of sweet memories. Your presence will make this day extra special! 🎀🎈",
+    "With hearts full of joy, we invite you and your little ones to celebrate our daughter's 1st birthday! Come join us for a magical afternoon of fun, laughter, cake, and lots of sweet memories. Your presence will make this day extra special! 🎀🎈",
 
   family: {
     heading: "Hosted By",
@@ -136,14 +136,14 @@ export const weddingData = {
 
   share: {
     whatsappText:
-      "🎉 You're invited to Prisha's 3rd Birthday Party! 🎀\n19 September 2026 | 4:00 PM | Yashobhoomi Society, Pune\nCome celebrate and make memories! 🎈\n",
+      "🎉 You're invited to Prisha's 1st Birthday Party! 🎀\n19 September 2026 | 4:00 PM | Yashobhoomi Society, Pune\nCome celebrate and make memories! 🎈\n",
     websiteUrl: "https://prisha-birthday-invitation.vercel.app/",
   },
 
   seo: {
-    title: "Prisha's 3rd Birthday 🎀 | You're Invited!",
+    title: "Prisha's 1st Birthday 🎀 | You're Invited!",
     description:
-      "You are invited to celebrate Prisha's 3rd Birthday on 19 September 2026 at Yashobhoomi Society, Pune.",
+      "You are invited to celebrate Prisha's 1st Birthday on 19 September 2026 at Yashobhoomi Society, Pune.",
     ogImage: "/images/og-preview.jpeg",
   },
 };
